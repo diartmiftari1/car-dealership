91c772e7611f4d6cbf62b2a3616f62f423c3f206
Diart Miftari <119237464+diartmiftari1@users.noreply.github.com>
2023-01-19 17:44:55 +0100

    database connection
    respekte king

---
+++ db_connection.php
@@ -0,0 +1,10 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "Test12345!";
+$database = "test1";
+
+$conn = mysqli_connect ($server,  $username, $password, $database  ) or die("not connected");
+echo "connected"
+?>
+
